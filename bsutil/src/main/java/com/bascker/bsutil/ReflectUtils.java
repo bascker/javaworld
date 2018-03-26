331b98c03134afab9fc86ba1c35ce7134d4e80f9
bascker <37108284+bascker@users.noreply.github.com>
2018-03-26 22:35:13 +0800

    修复 ReflectUtils

--- bsutil/src/main/java/com/bascker/bsutil/ReflectUtils.java
+++ bsutil/src/main/java/com/bascker/bsutil/ReflectUtils.java
@@ -1,7 +1,16 @@
 package com.bascker.bsutil;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 反射工具类
@@ -10,18 +19,44 @@ import java.lang.reflect.Method;
  */
 public class ReflectUtils {
 
-    public static Field getField (final Class clazz, final String fieldName) throws NoSuchFieldException {
-        final Field field = clazz.getDeclaredField(fieldName);
-        field.setAccessible(true);
+    private static final Logger LOG = LoggerFactory.getLogger(ReflectUtils.class);
 
-        return field;
+    public static Field getField (final Class clazz, final String fieldName) throws NoSuchFieldException {
+        return clazz.getDeclaredField(fieldName);
     }
 
     public static Method getMethod (final Class clazz, final String methodName) throws NoSuchMethodException {
-        final Method method = clazz.getDeclaredMethod(methodName);
-        method.setAccessible(true);
+        final List<Method> methods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> methodName.equals(method.getName()))
+                .collect(Collectors.toList());
 
-        return method;
+        return methods.get(0);
+    }
+
+    /**
+     * map 转换为指定类型的对象: 为了避免 mName 这种带固定前缀的属性名，从而走 setter 方法
+     * TODO: 性能问题, {@link ReflectUtils#getMethod(Class, String)} 每次找一个方法都是走一遍循环，性能较低
+     * @param map
+     * @param valueType
+     * @param <T>
+     * @return
+     */
+    public static <T> T map2Object (final Map<String, Object> map, final Class<T> valueType) throws IllegalAccessException, InstantiationException {
+        final T obj = valueType.newInstance();
+        final String METHOD_PREFIX = "set";
+
+        map.forEach((fieldName, fieldValue) -> {
+            final StringBuilder sb = new StringBuilder();
+            try {
+                final Method method = ReflectUtils.getMethod(valueType,
+                        sb.append(METHOD_PREFIX).append(StringUtils.capitalize(fieldName)).toString());
+                method.invoke(obj, fieldValue);
+            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                LOG.error(e.getMessage());
+            }
+        });
+
+        return obj;
     }
 
     private ReflectUtils () {}
--- bsutil/src/test/java/com/bascker/bsutil/ReflectUtilsTest.java
+++ bsutil/src/test/java/com/bascker/bsutil/ReflectUtilsTest.java
@@ -1,9 +1,16 @@
 package com.bascker.bsutil;
 
+import com.bascker.bsutil.bean.Person;
+import com.bascker.bsutil.bean.Sex;
+import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * ReflectUtils Unit Test
@@ -12,14 +19,31 @@ import java.lang.reflect.Method;
  */
 public class ReflectUtilsTest {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ReflectUtilsTest.class);
+
     @Test
     public void testGetMethod () throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         final Method method = ReflectUtils.getMethod(ReflectUtilsTest.class, "hello");
         final ReflectUtilsTest test = new ReflectUtilsTest();
         method.invoke(test);
+
+        final Method setName = ReflectUtils.getMethod(Person.class, "setName");
+        final Person person = new Person();
+        setName.invoke(person, "bascker");
+        LOG.info(person.toString());
+    }
+
+    @Test
+    public void testMap2Object () throws InstantiationException, IllegalAccessException {
+        final Map<String, Object> map = new HashMap<>();
+        map.put("name", "bascker");
+        map.put("age", 24);
+        map.put("sex", Sex.MALE);
+
+        Assert.assertEquals("Person{mName='bascker', mAge=24, mSex=MALE}", ReflectUtils.map2Object(map, Person.class).toString());
     }
 
     private void hello () {
-        System.out.println("I'm bascker");
+        LOG.info("I'm bascker");
     }
 }
