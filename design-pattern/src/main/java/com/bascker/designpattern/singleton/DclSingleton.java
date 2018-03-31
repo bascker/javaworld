cf808989c976a93a5db01ae5ee3f03452dac08d3
bascker <37108284+bascker@users.noreply.github.com>
2018-03-31 21:09:54 +0800

    单例模式

--- design-pattern/src/main/java/com/bascker/designpattern/singleton/DclSingleton.java
+++ design-pattern/src/main/java/com/bascker/designpattern/singleton/DclSingleton.java
@@ -3,7 +3,35 @@ package com.bascker.designpattern.singleton;
 import java.util.Objects;
 
 /**
- * volatile + DCL(双检测锁定) 实现懒汉式单例，解决 LazySample 高并发下效率问题
+ * volatile + DCL(Double Check Lock，双检测锁定) 实现懒汉式单例，解决 LazySample 高并发下效率问题
+ *
+ * 1.需要使用 volatile 修饰 mInstance, 以解决 DCL 的问题
+ * <p>
+ *     // 不给 mInstance 实例修饰 volatile 时 DCL 的问题
+ *     public class DclSingleton {
+ *
+ *         private static DclSingleton mInstance = null;
+ *         private int mNumber;
+ *
+ *         public static DclSingleton getInstance () {
+ *             // ...保持不变
+ *         }
+ *
+ *         public void getNumber {
+ *             return mNumber;
+ *         }
+ *
+ *        private DclSingleton () {
+ *            mNumber = 100;
+ *        }
+ *     }
+ *
+ *     DCL 问题: 多线程情况下，调用 getNumber() 时，返回值可能是 0.
+ *     原因: 在 getInstance() 的 if (Objects.isNull(mInstance)) 判断时，线程可能看到已经存在但尚未完全初始化(不完整)的实例.
+ *          即当线程 B 在执行 new DclSingle() 但内部的 mNumber 还未初始化为 100 时, 线程 A 开始执行 if 判断, 此时 A 会认为
+ *          mInstance 已经存在了，故而在调用 getNumber() 时，可能返回的是默认值 0.
+ * </p>
+ *
  *
  * @author bascker
  */
